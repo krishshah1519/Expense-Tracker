447953955e58fa6b78731fc667fd18f1750fce7f
krishshah1519 <krishshah1519@gmail.com>
2025-07-04 01:51:56 +0530

    deployement changes

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -19,7 +19,7 @@ function getCookie(name) {
 }
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL:  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
   withCredentials: true,
   headers: {
     'X-CSRFToken': getCookie('csrftoken'),
