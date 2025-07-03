394b592e4c492dd3d9dfcc6bf6d2cacb30e21dd4
krishshah1519 <krishshah1519@gmail.com>
2025-07-04 02:07:12 +0530

    deployement changes

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,29 +1,36 @@
 import axios from 'axios';
 
-
+// Get CSRF token from cookie
 function getCookie(name) {
   let cookieValue = null;
   if (document.cookie && document.cookie !== '') {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
-
-      if (cookie.substring(0, name.length + 1) === name + '=') {
+      if (cookie.startsWith(name + '=')) {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
     }
   }
-  console.log(cookieValue)
   return cookieValue;
 }
 
 const api = axios.create({
-  baseURL:  process.env.REACT_APP_API_BASE_URL ,
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',
   withCredentials: true,
-  headers: {
-    'X-CSRFToken': getCookie('csrftoken'),
-  },
+});
+
+// Attach CSRF token for safe HTTP methods (POST, PUT, DELETE)
+api.interceptors.request.use((config) => {
+  const csrfToken = getCookie('csrftoken');
+  if (
+    ['post', 'put', 'patch', 'delete'].includes(config.method) &&
+    csrfToken
+  ) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+  return config;
 });
 
 export default api;
